f8024ea4d7b993a6eeefbc977c0d454e0c5a718e
Moises <moises.hermosillo1510@gmail.com>
2025-01-14 14:06:56 -0600

    New account creation

---
+++ terraform/moises-mainserv1_bohemiancc.tf
@@ -0,0 +1,28 @@
+
+module "moises-mainserv1_bohemiancc_bp" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "moises+mainserv1@bohemian.cc"
+    AccountName               = "moises-mainserv1_bohemiancc"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "moises+mainserv1@bohemian.cc"
+    SSOUserFirstName          = ""
+    SSOUserLastName           = ""
+  }
+
+  account_tags = {
+    "BP departure control" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "BP departure control"
+    change_reason       = "Account created on BP departure control"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "bohecustom"
+}
